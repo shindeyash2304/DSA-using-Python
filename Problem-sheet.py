07a1ab66e88c71272973877288af56a4d8b2ffa2
CipherGhost23 <96590101+shindeyash2304@users.noreply.github.com>
2025-02-12 16:01:14 +0530

    Update Problem-sheet.py

--- Problem-sheet.py
+++ Problem-sheet.py
@@ -88,3 +88,81 @@ if marks >= 40:
 else:
     print("paisa barbad")
 
+# Practice Problems
+
+# Problem 1:
+# Write a Python program that takes 3 numbers as input and prints the largest among them.
+
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+c = int(input("Enter 3rd number:"))
+
+if a > b:
+    print ("a is largest")
+elif b > c:
+    print ("b is largest")
+else:
+    print ("c is largest")
+
+
+# Problem 2:
+# Write a program that checks whether a given year is a leap year or not. (A year is a leap year if it is divisible by 4 but not divisible by 100, except if it is also divisible by 400.)
+
+year = input("Enter the year: ")
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print (year, "is a leap year")
+        else:
+            print(year, "is not a leap year")
+    else:
+            print(year, "is a leap year")
+else:
+            print(year, "is not a leap year")
+            
+year = int(input("Enter the year: "))
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print(year, "is a leap year")
+            else:
+                print(year, "is not a leap year")
+        else:  # Divisible by 4 but not 100
+            print(year, "is a leap year")
+    else:
+        print(year, "is not a leap year")
+            
+# Problem 3:
+# Write a program that takes an integer as input and checks if it is positive, negative, or zero.
+
+num = int(input("Enter a number: "))
+if num > 0:
+    print (num, "is a positive")
+elif num == 0:
+    print ("zero")
+else:
+    print (num, "is a negative")
+
+# Problem 4:
+# Write a Python program that takes two numbers as input and performs division. If the second number is zero, print "Division by zero is not allowed."
+
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+if b == 0:
+    print ("Zero is not allowed")
+else:
+    div = a / b
+    print (div)
+
+
+# Problem 5:
+# Write a Python program that asks the user to enter a password and checks if it matches "Python123". If the password is correct, print "Access Granted", otherwise print "Access Denied".
+
+Password = "Python123"
+PW = input("Enter Password: ")
+
+if PW == Password :
+    print("Access Granted")
+else:
+    print("Acess Denied")
