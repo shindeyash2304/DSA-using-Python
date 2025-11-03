b4cb519dc50a4919d346f5cf1d1f4fbad6200994
CipherGhost23 <96590101+shindeyash2304@users.noreply.github.com>
2025-11-03 09:53:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Safe States/safe-states.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> safeNodes(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> revGraph(V);
+        vector<int> outdeg(V, 0);
+        for (auto &e : edges) {
+            int u = e[0], v = e[1];
+            revGraph[v].push_back(u);
+            outdeg[u]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < V; i++) {
+            if (outdeg[i] == 0) {
+                q.push(i);
+            }
+        }
+        vector<int> safe;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            safe.push_back(node);
+            for (int parent : revGraph[node]) {
+                outdeg[parent]--;
+                if (outdeg[parent] == 0){
+                    q.push(parent);
+                }
+            }
+        }
+        sort(safe.begin(), safe.end());
+        return safe;
+    }
+};
