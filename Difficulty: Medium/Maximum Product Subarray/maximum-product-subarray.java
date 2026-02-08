76b3b24f9af17ce3e9a6abdf0bf5641b66899d1d
CipherGhost23 <96590101+shindeyash2304@users.noreply.github.com>
2026-02-08 11:06:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    int maxProduct(int[] arr) {
+        // code here
+        int n = arr.length;
+        int ma = arr[0];
+        int mi = ma;
+        int ans = ma;
+        for(int i = 1; i < n; i++){
+            if(arr[i] < 0){
+               int temp = ma;
+               ma = mi;
+               mi = temp;
+            }
+            ma = Math.max(arr[i], arr[i]*ma);
+            mi = Math.min(arr[i], arr[i]*mi);
+            ans = Math.max(ans, ma);
+        }
+        
+        return ans;
+    }
+}
+
