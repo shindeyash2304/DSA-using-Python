031a8f14bb59ab76ec0c3a83a851bf556d2b7439
CipherGhost23 <96590101+shindeyash2304@users.noreply.github.com>
2026-03-11 13:55:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of subarray minimums/sum-of-subarray-minimums.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sumSubMins(self, arr):
+        n = len(arr)
+        left  = [0] * n
+        right = [0] * n
+
+        stack = []
+        for i in range(n):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            left[i] = i - stack[-1] if stack else i + 1
+            stack.append(i)
+
+        stack = []
+        for i in range(n - 1, -1, -1):
+            while stack and arr[stack[-1]] > arr[i]:
+                stack.pop()
+            right[i] = stack[-1] - i if stack else n - i
+            stack.append(i)
+
+        return sum(arr[i] * left[i] * right[i] for i in range(n))
