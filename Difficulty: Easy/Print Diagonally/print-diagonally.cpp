16ebb6bfd7ced9723d2e3dbd7d9d86d0f9bc97c9
CipherGhost23 <96590101+shindeyash2304@users.noreply.github.com>
2026-04-03 08:54:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Diagonally/print-diagonally.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> diagView(vector<vector<int>> mat) {
+        int n=mat.size();
+        map<int,vector<int>>mpp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                mpp[i+j].push_back(mat[i][j]);
+            }
+        }
+        vector<int>ans;
+        for(auto it:mpp){
+            for(auto x:it.second) ans.push_back(x);
+        }
+        return ans;
+    }
+};
+
